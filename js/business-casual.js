27703cc5ad81d68583d7ce58f3fae0f09564acdb
Jonathan Ballands <jballands@gmail.com>
2013-12-21 18:48:44 -0600

    Got all the heroes working (finally) with dynamic type setting. Also created some samples!

--- js/business-casual.js
+++ js/business-casual.js
@@ -10,6 +10,9 @@ window.onload = function() {
     /*
      *  Hero stuff
      */
+    
+    // Hero units
+    
     var heroUnitWidth = 0;
     var heroUnitHeight = 0;
 
@@ -26,15 +29,36 @@ window.onload = function() {
     for (var i = 0 ; i < heroUnitTextList.length ; i++) {
         var heroUnitText = heroUnitTextList[i];
         
-        var fontSize = window.getComputedStyle(heroUnitText, null).getPropertyValue("font-size");
-        
-        var heightOffset = (heroUnitHeight / 2) - (parseInt(fontSize) / 2);
+        var heightOffset = (heroUnitHeight / 2) - (heroUnitText.offsetHeight / 2);
         var widthOffset = (heroUnitWidth / 2) - (heroUnitText.offsetWidth / 2);
-        
-        console.log("Width offset = " + widthOffset + ", and heightOffset = " + heightOffset);
      
         heroUnitText.style.left = widthOffset;
         heroUnitText.style.top = heightOffset;
+    }
+    
+    // Sidekick units
+    
+    var sidekickUnitWidth = 0;
+    var sidekickUnitHeight = 0;
+
+    var sidekickUnitList =  document.querySelectorAll("div.sidekick-unit");
+    var sidekickUnitTextList =  document.querySelectorAll("span.sidekick-unit-text");
+
+    // Does a hero unit exist on the page?
+    if (sidekickUnitList.length > 0) {
+        // Just use the first one since they are all the same dimensions
+        sidekickUnitWidth = sidekickUnitList[0].offsetWidth;
+        sidekickUnitHeight = sidekickUnitList[0].offsetHeight;
+    }
+
+    for (var i = 0 ; i < sidekickUnitTextList.length ; i++) {
+        var sidekickUnitText = sidekickUnitTextList[i];
+        
+        var heightOffset = (sidekickUnitHeight / 2) - (sidekickUnitText.offsetHeight / 2);
+        var widthOffset = (sidekickUnitWidth / 2) - (sidekickUnitText.offsetWidth / 2);
+     
+        sidekickUnitText.style.left = widthOffset;
+        sidekickUnitText.style.top = heightOffset;
     }
     
 };
